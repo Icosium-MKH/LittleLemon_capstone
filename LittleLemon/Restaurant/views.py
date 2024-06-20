04252ada01868c23ca4262dc4c863bca75c649ae
Malik <malik.khetim@gmail.com>
2024-06-20 16:28:18 +0200

    menu and booking views added

--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView 
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .models import booking,menu
+from .serializers import bookingSerializer, menuSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+"""
+class bookingitemview(ListCreateAPIView):
+
+    def get(self,request):
+        items = booking.objects.all()
+        serializer = bookingSerializer(items, many=True)
+        return Response(serializer.data) # Return JSON
+    
+
+    def post(self,request):
+        serializer = bookingSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success","data":"serializer.data"})
+"""        
+
+class MenuItemView(ListCreateAPIView):
+    queryset = menu.objects.all()
+    serializer_class = menuSerializer
+
+        
+
+class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
+    queryset = menu.objects.all()
+    serializer_class = menuSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = booking.objects.all()
+    serializer_class = bookingSerializer
