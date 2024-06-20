c577964f36b2c563e91230dac69517b3e0baf730
Malik <malik.khetim@gmail.com>
2024-06-20 15:01:50 +0200

    added models

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator
+# import datetime
 # Create your models here.
+
+class booking (models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(validators=[MaxValueValidator(6)])
+    bookingdate = models.DateTimeField()
+
+
+class menu (models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    inventory = models.IntegerField(validators=[MaxValueValidator(5)])
+
+
